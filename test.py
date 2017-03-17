60eb26fb7b5f8955f8e99d833b90759169a866e4
thcoffee <48222283@qq.com>
2017-03-17 15:45:39 +0800

    ceshi
    zheshidiyici

---
+++ test.py
@@ -0,0 +1,20 @@
+class Books(object):
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return self.title
+
+    def __repr__(self):
+        return self.title
+
+    def __call__(self):
+    
+        print ("%s is written by %s"%(self.title, self.author))
+
+
+if __name__ == '__main__':
+    pybook = Books('Core Python', 'Wesley')
+    print(pybook)
+    pybook()
