1c4eb97451be73840ccd7424af2c3d342e1d0794
thcoffee <48222283@qq.com>
2017-03-17 15:49:11 +0800

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,9 @@
+from test import *
+
+if __name__=="__main__":
+    teacher=Teacher('mary')
+    print(teacher._name)
+    print(teacher.getName())
+    print(teacher._getName())
+    print(a)
+    print(_a)
